4cf17237c9a81a67abd0ca10711aa48eac5360fc
Chris Wright <chris@inkyspider.co.uk>
2017-05-04 17:45:44 +0100

    Added load value default in main and added load to plot title

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import plotly.graph_objs as go
 def main(rest_angle):
     press = physics.System()
 
+    press.rest_angle = rest_angle
+    press.load = 2
+
     press.spring.constant = 1630
     press.spring.free_length = 0.135
     press.spring.minimum_length = 0.0388
@@ -79,10 +82,12 @@ def main(rest_angle):
 
     data = [trace_compression, trace_spring_force, trace_press_bc, trace_press_ec, trace_press_be_minimum, trace_spring_force_maximum]
 
-    figure = {"data" : data, "layout" : {"title": "Screen Press System Analysis - Rest angle: {}".format(press.rest_angle)}}
+    figure = {"data" : data, "layout" : {"title": "Screen Press System Analysis - Rest angle: {ang}, {load}kg load".format(
+        ang=press.rest_angle, load=press.load)}}
 
     plotly.offline.plot(figure, filename="{load}kg-load_{Sk}Nm-1_{ang}deg-rest-angle.html".format(
         Sk=press.spring.constant, ang=press.rest_angle, load=press.load))
-    
+
+
 for angle in [25, 30, 35]:
     main(angle)
